d8662dc4d9164a4910aaee5c2aa2ed11e4499882
Andreas Gawecki <andreas.gawecki@coremedia.com>
2020-11-20 14:03:57 +0100

    print java version

--- src/test/Main.java
+++ src/test/Main.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(System.getProperty("java.runtime.name"));
+    System.out.println(System.getProperty("java.version"));
 
     String imageFileName = "test.png";
     try {
